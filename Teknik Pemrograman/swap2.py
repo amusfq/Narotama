152d5f31c1899060bec1c9df482c44b56dbf2a6c
amusfq <amusfq@gmail.com>
2020-02-17 12:22:35 +0700

    Program swap jenis 2

---
+++ Teknik Pemrograman/swap2.py
@@ -0,0 +1,15 @@
+bil1 = input("Masukkan bilangan #1 : ")
+bil2 = input("Masukkan bilangan #2 : ")
+
+print("Bilangan sebelum di swap")
+print("Bilangan #1 :", bil1)
+print("Bilangan #2 :", bil2)
+
+bil3 = bil1
+bil1 = bil2
+bil2 = bil3
+
+print()
+print("Bilangan setelah di swap")
+print("Bilangan #1 :", bil1)
+print("Bilangan #2 :", bil2)
