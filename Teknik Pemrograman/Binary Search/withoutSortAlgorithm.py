03238fcb581393ca62dca3a594f81dc141117fc0
amusfq <amusfq@gmail.com>
2020-04-13 10:46:28 +0700

    Menambah tugas Binary Search

---
+++ Teknik Pemrograman/Binary Search/withoutSortAlgorithm.py
@@ -0,0 +1,64 @@
+import time
+import os
+
+def start():
+    while True:
+        os.system('clear')
+        print("========================================")
+        print("             BILANGAN ARRAY             ")
+        print(" [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]")
+        print("                                        ")
+        print("   Masukkan Bilangan Yang Akan Dicari   ")
+        print("                                        ")
+        print("========================================")
+        arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        x = input()
+        if x.isnumeric():
+            def binSearch(array, kiri, kanan, target):
+                while kiri <= kanan:
+                    tengah = kiri + (kanan - kiri) // 2
+                    if int(array[tengah]) == target:
+                        return tengah
+                    elif int(array[tengah]) < target:
+                        kiri = tengah + 1
+                    else:
+                        kanan = tengah - 1
+                return -1
+
+            start = time.time()
+            hasil = binSearch(arr, 0, len(arr) -1, int(x))
+            end = time.time()
+
+            if hasil != -1:
+                print("Bilangan {} ada di array index ke {}" .format(x, hasil))
+            else:
+                print("Bilangan {} tidak ada didalam array" .format(x))
+
+            print("Selesai dalam %f detik" % (end - start))
+            input("(Enter untuk melanjutkan)")
+            return False
+        else:
+            print("Data yang anda masukkan bukan angka")
+            input("(Enter untuk mengulangi)")
+
+while True:
+    os.system('clear')
+    print("=============================")
+    print("=       BINARY SEARCH       =")
+    print("=                           =")
+    print("=  1. Start Program        =")
+    print("=  2. Exit                  =")
+    print("=                           =")
+    print("=          CREATOR          =")
+    print("=  Achmad Musyaffa Taufiqi  =")
+    print("    Teknik Informatika A    ")
+    print("          04319023          ")
+    print("=============================")
+    pil = int(input("Masukkan pilihan: "))
+    if pil == 1 :
+        start()
+    elif pil == 2:
+        exit()
+    else:
+        print("Pilihan salah..")
+        input("(Enter untuk melanjutkan)")
